094480b1acda3fce521c067ff195fd4e47a742ac
김찬호 <90chanho@gmail.com>
2020-05-31 18:47:08 +0900

    add lib file cookie.js

---
+++ src/assets/lib/cookie.js
@@ -0,0 +1,15 @@
+export function getCookie(cname) {
+	const name = cname + "="
+	const decodedCookie = decodeURIComponent(document.cookie)
+	const ca = decodedCookie.split(";")
+	for (var i = 0; i < ca.length; i++) {
+		var c = ca[i]
+		while (c.charAt(0) === " ") {
+			c = c.substring(1)
+		}
+		if (c.indexOf(name) === 0) {
+			return c.substring(name.length, c.length)
+		}
+	}
+	return c
+}
